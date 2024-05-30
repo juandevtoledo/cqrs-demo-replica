10f54b7877515d7a9555612bd63a08fbd49d51bb
juanDevtoledoMeli <juan.toledo@mercadolibre.com.co>
2024-05-30 17:37:45 -0500

    first commit

---
+++ src/main/java/com/example/cqrsdemoreplica/CqrsDemoReplicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cqrsdemoreplica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsDemoReplicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsDemoReplicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cqrsdemoreplica/CqrsDemoReplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cqrsdemoreplica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsDemoReplicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
